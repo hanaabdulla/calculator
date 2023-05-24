e578f418c0d07f12e12541eb5fc57afd837feaa7
hanaabdulla <hanaabdulla@ug.cusat.ac.in>
2023-05-24 15:43:41 +0530

    modified

--- cal.js
+++ cal.js
@@ -2,22 +2,32 @@
 let screenEL = document.getElementById("screen");
 let screenValue = "";
 
-function press(num){
+function press(num) {
     screenValue += num;
     screenEL.value = screenValue;
 }
-function clearscreen(){
- 
-  screenValue="";
+function back() {
+    let a = screenValue;
+    let b = a.slice(0, -1);
+    screenValue = b;
+    screenEL.value = b;
+
+
+}
+function clearscreen() {
+
+    screenValue = "";
     screenEL.value = screenValue;
 }
-function equal(){
+
+
+function equal() {
     let a = screenEL.value;
     let b = eval(a);
     screenEL.value = b;
     screenValue = b;
 }
-   
 
-   
+
+
 
